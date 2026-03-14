d42b5709de3e558a2c908dfa651d1aba9caa42c8
Deelaka Herath <herathdeelaka730@gmail.com>
2026-03-14 10:39:48 +0530

    updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // Tailwind වැඩ කරන්න මේක අනිවාර්යයි
+import './index.css' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
