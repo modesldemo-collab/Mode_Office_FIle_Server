a7199fec38d45423fd64fbbf0484789b4f84a2e9
Deelaka Herath <herathdeelaka730@gmail.com>
2026-03-16 10:36:39 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import Dashboard from './pages/Dashboard'; 
+import Dashboard from './pages/Dashboard';
+import Myfiles from './pages/MyFiles'; 
 
 function App() {
   return (
     <div className="App">
-      <Dashboard />
+      <Myfiles />
     </div>
   );
 }
